1d1d1c9ad1cf37a4e963f6b393168a4afff43388
Zensi77 <juanmaespi7@gmail.com>
2025-05-29 22:12:39 +0200

    integracion con vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 import react from '@astrojs/react';
 
@@ -16,7 +17,11 @@ export default defineConfig({
     },
   },
   output: 'server',
-
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   build: {
     inlineStylesheets: 'auto',
   },
